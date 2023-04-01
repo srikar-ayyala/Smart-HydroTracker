f98537d874cfbbf7d06116253a8fe8ed5620063b
Ayyala Sai Srikar <srikar.ayyala3@gmail.com>
2023-04-01 11:10:23 +0530

    update nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import NavBar from "./components/navBar/NavBar"
+import Hero from "./components/hero/Hero"
 import './app.css'
 function App() {
 
   return (
     <div className="main-page">
-      <NavBar />
+        <NavBar />
+        <Hero />
+        <div className="spacer" style={{height: '5000px'}}></div>
     </div>
   )
 }
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,7 +2,9 @@ import logoPath from '../../../public/logo.png'
 import './navBar.css'
 export default function NavBar() {
     return <nav>
-        <img src={logoPath} alt="Logo" />
+        <div className="logo-box">
+            <img src={logoPath} alt="Logo" />
+        </div>
         <div className="nav-links">
             <a href="#" className='nav-link'>About</a>
             <a href="#" className='nav-link'>FAQ</a>
