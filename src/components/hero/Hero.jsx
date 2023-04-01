6cc800a1a0b3836d97af08f02569da60cf7a16a5
Ayyala Sai Srikar <srikar.ayyala3@gmail.com>
2023-04-01 11:09:51 +0530

    add hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,8 @@
+import './hero.css'
+
+export default function Hero() {
+    return <section className="hero-section">
+        <h1>IOT HYDROMETER</h1>
+        {/* <h1>IOT Hydrometer</h1> */}
+    </section>
+}
