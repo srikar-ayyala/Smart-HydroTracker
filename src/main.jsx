19ecaef706b87cd62db2b7c2af531f973014df33
Ayyala Sai Srikar <srikar.ayyala3@gmail.com>
2023-04-04 08:18:03 +0530

    adding react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Dashboard from './pages/dashboard/Dashboard';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Something went wrong...</h1>
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
