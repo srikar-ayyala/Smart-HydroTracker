56728272956652569dc4e56d927fa0afcb3ee3a8
Ayyala Sai Srikar <srikar.ayyala3@gmail.com>
2023-04-16 18:36:31 +0530

    Handle 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,11 @@ const router = createBrowserRouter([
     path: 'dashboard',
     element: <Dashboard />,
     errorElement: <h1>Something went wrong...</h1>
+  },
+  {
+    path: '*',
+    element: <Dashboard />,
+    errorElement: <h1>Something went wrong...</h1>
   }
 ])
 
