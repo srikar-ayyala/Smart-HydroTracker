e12a9a542a4ceed67463c2897d92e90b6b423e9e
Ayyala Sai Srikar <srikar.ayyala3@gmail.com>
2023-04-16 17:53:17 +0530

    Fixing link at Go To Dashboard section

--- src/components/goToDashboard/GoToDashboard.jsx
+++ src/components/goToDashboard/GoToDashboard.jsx
@@ -1,9 +1,11 @@
 import './goToDashboard.css'
+import { Link } from 'react-router-dom'
 
 export default function GoToDashboard() {
     return <section className="go-to-dashboard-section">
         <h2>Start managing your water today</h2>
         {/* <p>what gets measured gets managed</p> */}
-        <div className="go-to-dashboard-btn">Dashboard</div>
+        {/* <div className="go-to-dashboard-btn">Dashboard</div> */}
+        <Link to={'/dashboard'} className='go-to-dashboard-btn'>Dashboard</Link>
     </section>
 }
